f1dbe748966aa91ac2602770e7bb04d3406fc10a
kenta7777 <k.hasegw7@gmail.com>
2021-11-03 20:13:23 +0900

    post form data and json body

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate reqwest;
 
+use std::collections::HashMap;
 use reqwest::Response;
 
 #[tokio::main]
 async fn main()-> Result<(), reqwest::Error> {
     get_body().await;
     post_body().await;
+    post_form_body().await;
+    post_json_body().await;
     Ok(())
 }
 
@@ -22,6 +25,32 @@ async fn post_body() -> Result<(), reqwest::Error> {
     println!("post");
     let client = reqwest::Client::new();
     let response = client.post("http://httpbin.org/post").body("the body is posted").send().await?;
+    
+    println!("response: {:?}", response);
+    println!("status: {:?}", response.status());
+    Ok(())
+}
+
+// post the body of `name=alice&age=12` with RequestBuilder form()
+async fn post_form_body() -> Result<(), reqwest::Error> {
+    println!("post form data");
+    let params = [("name", "alice"), ("age", "12")];
+    let client = reqwest::Client::new();
+    let response = client.post("http://httpbin.org/post").form(&params).send().await?;
+    
+    println!("response: {:?}", response);
+    println!("status: {:?}", response.status());
+    Ok(())
+}
+
+// post the body of json (`{"name": "alice", "age": "14"}`) with RequestBuilder json()
+async fn post_json_body() -> Result<(), reqwest::Error> {
+    println!("post json data");
+    let mut json_map = HashMap::new();
+    json_map.insert("name", "alice");
+    json_map.insert("age", "12");
+    let client = reqwest::Client::new();
+    let response = client.post("http://httpbin.org/post").json(&json_map).send().await?;
     
     println!("response: {:?}", response);
     println!("status: {:?}", response.status());
