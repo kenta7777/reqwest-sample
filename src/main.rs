179781e0b23d1d11859f83165801747de92c13f8
kenta7777 <k.hasegw7@gmail.com>
2021-11-03 16:52:39 +0900

    post to httpbin

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use reqwest::Response;
 #[tokio::main]
 async fn main()-> Result<(), reqwest::Error> {
     get_body().await;
+    post_body().await;
     Ok(())
 }
 
@@ -14,5 +15,15 @@ async fn get_body() -> Result<(), reqwest::Error> {
     let body = response.text().await?;
     println!("response: {:?}", body);
 
+    Ok(())
+}
+
+async fn post_body() -> Result<(), reqwest::Error> {
+    println!("post");
+    let client = reqwest::Client::new();
+    let response = client.post("http://httpbin.org/post").body("the body is posted").send().await?;
+    
+    println!("response: {:?}", response);
+    println!("status: {:?}", response.status());
     Ok(())
 }
